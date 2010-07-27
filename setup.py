75fbed75bc4462dd6fe7226cc83f8af6ea39e55e
Jeffrey Gelens <none@none>
2010-07-28 00:29:48 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gevent-websocket",
+    version="0.1.0",
+    description="Websocket handler for the gevent pywsgi server, a Python network library",
+    long_description=open("README.rst").read(),
+    author="Jeffrey Gelens",
+    author_email="jeffrey@noppo.org",
+    url="",
+    install_requires=("gevent", "greenlet"),
+    packages=find_packages(exclude=["example","test"]),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Topic :: Internet",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+    ],
+)

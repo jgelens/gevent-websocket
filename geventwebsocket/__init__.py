beb62ce43acbc76e7dbaf18c1b0d01cc4acb8eb2
Denis Bilenko <denis.bilenko@gmail.com>
2011-12-30 19:10:17 +0600

    add WebSocketError to __init__.py

--- geventwebsocket/__init__.py
+++ geventwebsocket/__init__.py
@@ -2,6 +2,7 @@
 version_info = (0, 3, 0, 'dev')
 __version__ = ".".join(map(str, version_info))
 
-__all__ = ['WebSocketHandler']
+__all__ = ['WebSocketHandler', 'WebSocketError']
 
 from geventwebsocket.handler import WebSocketHandler
+from geventwebsocket.websocket import WebSocketError

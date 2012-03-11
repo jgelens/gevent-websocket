f3c600d366d2ee4bb08cb2562a647f42f9cd3222
Jeffrey Gelens <jeffrey@gelens.org>
2012-03-11 10:33:04 +0100

    Removed old files

--- geventwebsocket/__init__.py
+++
@@ -1,9 +0,0 @@
-
-version_info = (0, 2, 2)
-__version__ =  ".".join(map(str, version_info))
-
-try:
-    from geventwebsocket.websocket import WebSocket
-except ImportError:
-    import traceback
-    traceback.print_exc()

a836a3a204702ca7a1d6092b9f4805a6c153f534
Jeffrey Gelens <jeffrey@noppo.pro>
2013-04-10 16:15:54 +0200

    First draft of new WebSocketApplication layer.

--- examples/wamp.py
+++ examples/wamp.py
@@ -1,5 +1,5 @@
-from gevent import pywsgi
-from geventwebsocket.handler import WebSocketHandler
+from geventwebsocket.server import WebSocketServer
+from geventwebsocket.resource import MessageResource, WebSocketApplication
 from geventwebsocket.protocols.wamp import WampProtocol, export_rpc
 
 
@@ -9,19 +9,37 @@ class RPCTestClass(object):
         return x * y
 
 
-class AdditionServer(WampProtocol):
+class WampApplication(WebSocketApplication):
     def on_open(self):
-        self.register_procedure("http://localhost:8000/calc#add", self.add)
-        self.register_object("http://localhost:8000/test#", RPCTestClass)
+        self.wamp.register_procedure("http://localhost:8000/calc#add", self.add)
+        self.wamp.register_object("http://localhost:8000/test#", RPCTestClass)
+        self.wamp.send_welcome()
+        print "opened"
+
+    def on_message(self, message):
+        print "message: ", message
+        super(WampApplication, self).on_message(message)
+
+    def on_close(self):
+        print "closed"
 
     def add(self, var, has):
         has.update({'bloep': var})
         return has
 
+    def build_protocol(self):
+        self.wamp = WampProtocol(self)
+        return self.wamp
 
-def app(environ, start_response):
-    AdditionServer(environ)
+    @classmethod
+    def supported_protocols(cls):
+        return [WampProtocol.PROTOCOL_NAME]
+
 
 if __name__ == "__main__":
-    server = pywsgi.WSGIServer(("", 8000), app, handler_class=WebSocketHandler)
+    resource = MessageResource(apps={
+        '/': WampApplication
+    })
+
+    server = WebSocketServer(("", 8000), resource, protocols=resource.supported_protocols,  debug=True)
     server.serve_forever()

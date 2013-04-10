e1526ac548b491cd3858e01b3739734a0c7cd365
Jeffrey Gelens <jeffrey@noppo.pro>
2013-04-10 09:37:20 +0200

    Added possibility to define a pre_start_hook

--- geventwebsocket/handler.py
+++ geventwebsocket/handler.py
@@ -39,7 +39,11 @@ class WebSocketHandler(WSGIHandler):
             self.websocket.close()
 
     def run_application(self):
-        self.logger.debug("Application started")
+        if self.server.pre_start_hook:
+            self.logger.debug("Calling pre-start hook")
+            self.server.pre_start_hook(self)
+
+        self.logger.debug("Initializing WebSocket")
         self.result = self.upgrade_websocket()
 
         if hasattr(self, 'websocket'):
@@ -224,29 +228,3 @@ class WebSocketHandler(WSGIHandler):
         self.close_connection = True
         # prevents the Date header from being written
         self.provided_date = True
-
-
-
-#class MessageHandler(object):
-#    def __init__(self, environ, interfaces):
-#        self.ws = environ['wsgi.websocket']
-#        self.interfaces = interfaces
-#
-#        if self.ws.path in interfaces.keys():
-#            self.active_interface = interfaces[self.ws.path]
-#        else:
-#            raise Exception("no interface found")
-#
-#        self.on_open()
-#        self._handle()
-#
-#    def _handle(self):
-#        while True:
-#            message = self.ws.receive()
-#
-#            if message is None:
-#                self.active_interface.on_close()
-#                break
-#            else:
-#                self.active_interface.on_message(message)
-

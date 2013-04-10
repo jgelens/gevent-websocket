96c0276b21d89ac69f1048cf25bdc63be9206259
Jeffrey Gelens <jeffrey@gelens.org>
2013-04-10 12:35:12 +0200

    Updated setup.py to get version from get_version

--- setup.py
+++ setup.py
@@ -1,25 +1,30 @@
 from setuptools import setup, find_packages
 
+
+version = __import__('geventwebsocket').get_version()
+
 setup(
     name="gevent-websocket",
-    version="0.3.6",
-    description="Websocket handler for the gevent pywsgi server, a Python network library",
-    long_description=open("README.rst").read(),
+    version=version,
+    url="https://bitbucket.org/Jeffrey/gevent-websocket",
     author="Jeffrey Gelens",
     author_email="jeffrey@noppo.pro",
-    license="BSD",
-    url="https://bitbucket.org/Jeffrey/gevent-websocket",
+    description=("Websocket handler for the gevent pywsgi server, a Python "
+                 "network library"),
+    long_description=open("README.rst").read(),
     download_url="https://bitbucket.org/Jeffrey/gevent-websocket",
     install_requires=("gevent", "greenlet"),
-    packages=find_packages(exclude=["examples","tests"]),
+    packages=find_packages(exclude=["examples", "tests"]),
+    license="BSD",
     classifiers=[
         "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX",
+        "Programming Language :: Python",
         "Topic :: Internet",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        "Intended Audience :: Developers",
-    ],
+    ]
 )

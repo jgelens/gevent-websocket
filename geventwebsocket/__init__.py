9a3d4022afb20980084c8e27f58d8a71b05e6657
Jeffrey Gelens <jeffrey@gelens.org>
2013-04-16 14:37:09 +0200

    Wamp fixes

--- geventwebsocket/__init__.py
+++ geventwebsocket/__init__.py
@@ -1,14 +1,21 @@
 VERSION = (0, 4, 0, 'alpha', 0)
 
+__all__ = [
+    'WebSocketApplication',
+    'Resource',
+    'WebSocketServer',
+    'WebSocketError',
+    'get_version'
+]
+
 
 def get_version(*args, **kwargs):
     from .utils import get_version
     return get_version(*args, **kwargs)
 
-
-from .resource import WebSocketApplication, Resource
-from .server import WebSocketServer
-from .exceptions import WebSocketError
-
-__all__ = [WebSocketApplication, Resource, WebSocketServer, WebSocketError,
-           get_version]
+try:
+    from .resource import WebSocketApplication, Resource
+    from .server import WebSocketServer
+    from .exceptions import WebSocketError
+except ImportError:
+    pass

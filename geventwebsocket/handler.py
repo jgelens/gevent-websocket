bad24295a13fd88baab56292a488db1c29a85461
Rogier Koppejan <rogier.koppejan@gmail.com>
2018-11-21 13:16:42 +0100

    allow empty keys

--- geventwebsocket/handler.py
+++ geventwebsocket/handler.py
@@ -53,7 +53,7 @@ class WebSocketHandler(WSGIHandler):
                 self.client_address, self.websocket)
             list(self.application(self.environ, lambda s, h, e=None: []))
         finally:
-            if self.client_address:
+            if self.client_address in self.server.clients:
                 del self.server.clients[self.client_address]
             if not self.websocket.closed:
                 self.websocket.close()

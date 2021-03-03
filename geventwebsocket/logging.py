aa49991b77c42c53d3e50efe111bb31492f5de01
Adamos Kyriakou <somada141@gmail.com>
2021-03-03 10:12:51 +0200

    [MQ-682] Fixed over-indentation.

--- geventwebsocket/logging.py
+++ geventwebsocket/logging.py
@@ -1,31 +1,32 @@
-
-
 from logging import getLogger, StreamHandler, getLoggerClass, Formatter, DEBUG
 
 
+# noinspection PyShadowingBuiltins
 def create_logger(name, debug=False, format=None):
-        Logger = getLoggerClass()
+    Logger = getLoggerClass()
 
-        class DebugLogger(Logger):
-            def getEffectiveLevel(x):
-                if x.level == 0 and debug:
-                    return DEBUG
-                else:
-                    return Logger.getEffectiveLevel(x)
+    # noinspection PyMethodParameters
+    class DebugLogger(Logger):
+        def getEffectiveLevel(x):
+            if x.level == 0 and debug:
+                return DEBUG
+            else:
+                return Logger.getEffectiveLevel(x)
 
-        class DebugHandler(StreamHandler):
-            def emit(x, record):
-                StreamHandler.emit(x, record) if debug else None
+    # noinspection PyMethodParameters
+    class DebugHandler(StreamHandler):
+        def emit(x, record):
+            StreamHandler.emit(x, record) if debug else None
 
-        handler = DebugHandler()
-        handler.setLevel(DEBUG)
+    handler = DebugHandler()
+    handler.setLevel(DEBUG)
 
-        if format:
-            handler.setFormatter(Formatter(format))
+    if format:
+        handler.setFormatter(Formatter(format))
 
-        logger = getLogger(name)
-        del logger.handlers[:]
-        logger.__class__ = DebugLogger
-        logger.addHandler(handler)
+    logger = getLogger(name)
+    del logger.handlers[:]
+    logger.__class__ = DebugLogger
+    logger.addHandler(handler)
 
-        return logger
+    return logger

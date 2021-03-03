63e4a9ed4e585139a773811b6149e9999d6e642b
Adamos Kyriakou <somada141@gmail.com>
2021-03-03 10:14:08 +0200

    [MQ-682] Updated compatibility-layer module to error under Python 2.

--- geventwebsocket/_compat.py
+++ geventwebsocket/_compat.py
@@ -1,23 +1,17 @@
-
-
 import sys
-import codecs
 
 
 PY3 = sys.version_info[0] == 3
 PY2 = sys.version_info[0] == 2
 
-
 if PY2:
-    bytes = str
-    text_type = str
-    string_types = str
-    range_type = xrange
-    iteritems = lambda x: iter(x.items())
-    # b = lambda x: x
+    raise NotImplementedError(
+        "This version of `gevent-websocket` does not support Python 2.x"
+    )
 else:
     text_type = str
     string_types = str,
     range_type = range
+    # noinspection PyPep8
     iteritems = lambda x: iter(list(x.items()))
     # b = lambda x: codecs.latin_1_encode(x)[0]
